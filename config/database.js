b2fc4655580718ed608679a702386d55ebbcc85a
yunju-jang <yjjang305@naver.com>
2021-07-22 22:51:04 +0900

    210722

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host     : "localhost",
+    user     : "root",
+    password : "wet2021",
+    database : "wet2021",
+    port     : 3306
+};
---
+++ main.js
@@ -0,0 +1,39 @@
+var mysql         = require('mysql'),
+    express       = require('express'),
+    http          = require('http'),
+    path          = require('path'),
+    bodyParser    = require('body-parser'),
+    fs            = require('fs'),
+    routes        = require('./routes'),
+    user          = require('./routes/user'),
+    dbconfig      = require('./config/database.js'),
+    connection    = mysql.createConnection(dbconfig);
+
+var app = express();
+
+app.use(bodyPars.urlencoded({extended: false}));
+app.get('/', (req, res) => {
+    console.log(req.query);
+    res.send({"result": "GET 호출"});
+})
+
+app.post(`/`, (req, res) => {
+  console.log(req.body);
+  res.send({"result": "POST 호출"});
+})
+
+app.put(`/:id`, (req, res) => {
+  console.log(`내용 PrimaryKey : ${req.params.id}`)
+  console.log(req.body);
+  res.send({"result": "UPDATE 호출"});
+})
+
+app.delete(`/:id`, (req, res) => {
+  console.log(req.params.id);
+  console.log(req.path)
+  res.send({"result": "DELETE 호출"});
+})
+
+app.listen(3000,  function () {
+    console.log('서버 실행 중...');
+});
